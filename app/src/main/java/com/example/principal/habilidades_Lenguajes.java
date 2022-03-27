0f4309048962a2f934e17a9ce7c5675f7ad019a6
Johanna Andrade Rivera <andrader.johanna@gmail.com>
2022-03-26 23:24:08 -0500

    Versión 1 Hoja de vida Johanna

---
+++ app/src/main/java/com/example/principal/habilidades_Lenguajes.java
@@ -0,0 +1,14 @@
+package com.example.principal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class habilidades_Lenguajes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habilidades_lenguajes);
+    }
+}
